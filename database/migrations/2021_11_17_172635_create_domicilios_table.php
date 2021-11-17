740ea22f667a663cad5db1298b2b053676517d78
Tatiana <tatiana_lopez_tsapp1ta2021@cev.com>
2021-11-17 18:42:21 +0100

    domicilioController y migration

---
+++ database/migrations/2021_11_17_172635_create_domicilios_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDomiciliosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('domicilios', function (Blueprint $table) {
+            $table->id();
+            $table->string('calle',250);
+            $table->integer('numero')->unsigned();
+            $table->integer('codigoPostal')->unsigned();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('domicilios');
+    }
+}
